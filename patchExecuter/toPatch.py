d439beb7b2328832c514832f36295ab55aa1d72b
Elad Rosenberger <eladr@eladr-mac.local>
2021-02-18 12:17:17 +0200

    update script

--- patchExecuter/toPatch.py
+++ patchExecuter/toPatch.py
@@ -49,6 +49,12 @@ def getListOfFilesByName(fname, dir):
             returnList.append(f)
     return returnList
 
+def printInfoToLog(text):
+    print("[INFO] - {}".format(text))
+
+def printErrorToLog(text):
+    print("[ERROR] - {}\nFAILED".format(text))
+
 
 '''
 # This Script Archive a list of Java classes
@@ -60,55 +66,81 @@ def getListOfFilesByName(fname, dir):
 _datetime=datetime.now().strftime("%d%m%Y_%H%M%S")
 suffix = ".java"
 logFile = "readme.log"
+_type = "PROD"
+
 '''
     Getting params from user
 '''
 try:
     _fileName=sys.argv[1]
 except:
-    print("First argument is mandatory, and should be the 'fileName' contain all required classes")
+    printErrorToLog("First argument is mandatory, and should be the 'fileName' contain all required classes")
     exit()
 try:
     _prodNumber=sys.argv[2]
 except:
-    print("Second argument is mandatory, and should be 'PROD/SD number'")
+    printErrorToLog("Second argument is mandatory, and should be 'PROD/SD number'")
     exit()
 try:
     _buildNumber=sys.argv[3]
 except:
-    print("Third argument is mandatory, and should be 'Build number'")
+    printErrorToLog("Third argument is mandatory, and should be 'Build number'")
     exit()
 
+'''
+    Validate param inputs
+'''
+if not isFileExist(_fileName.strip()):
+    printErrorToLog("File name {} doesn't exist in current directory!".format(_fileName))
+    exit()
+# Patch Type - SD or PROD
+if "=" not in _prodNumber:
+    printErrorToLog("Ticket number of the patch should be in p/s=xxxx format. (example p=0012 where p-PROD, s-SD)")
+    exit()
+else:
+    _type = _prodNumber.split("=")[0].lower()
+    if _type != "p" and _type != "s":
+        printErrorToLog("Patch type must be from a type: s-SD / p-PROD")
+        exit()
+    _type = "PROD" if _type == "p" else "SD"
+    _prodNumber = _prodNumber.split("=")[1]
+    printInfoToLog("type: {}, number: {}".format(_type, _prodNumber))
+
+if "=" not in _buildNumber:
+    printErrorToLog("Build number of the patch should be in b=xxxx format. (example b=0012 where b-build number)")
+    exit()
+else:
+    if _buildNumber.split("=")[0] != "b":
+        printErrorToLog("Build number of the patch should be in b=xxxx format. (example b=0012 where b-build number )")
+        exit()
+    _buildNumber = _buildNumber.split("=")[1]
+    printInfoToLog("build number: {}".format(_buildNumber))
 
 '''
     Initialize the log file
 '''
 log_file = open(logFile,"w")
 sys.stdout = log_file
-print()
-print("Start to create a Patch by %s file, PROD/SD=%s, BUILD=%s]"%(_fileName, _prodNumber, _buildNumber))
-
+printInfoToLog("Cellwize Patch Executer\n")
+printInfoToLog("Created time: {}".format(datetime.now().strftime("%d-%m-%Y %H:%M:%S")))
+printInfoToLog("Start to create a Patch by %s file. params [PROD/SD=%s, BUILD=%s]"%(_fileName, _prodNumber, _buildNumber))
 
 '''
     Start Here
 '''
-if not isFileExist(_fileName.strip()):
-    print("File name {} doesn't exist in current directory!\nFAILED".format(_fileName))
-    exit()
-
 # create base directory for the patch
-_baseDir = "Patch.PROD_" + str(_prodNumber) + ".build_" + str(_buildNumber) + "." + _datetime
+_baseDir = "Patch.{}_{}.build_{}.{}".format(_type, str(_prodNumber), str(_buildNumber), _datetime)
 if not createDir(_baseDir):
-    print( "couldn't create a new directory {}\nFAILED".format(_baseDir))
+    printErrorToLog( "couldn't create a new directory {}".format(_baseDir))
 # open file and loop over classes name
 with open(_fileName) as f:
     classFiles = f.readlines()
-    print("Packing the following Classes:\n")
+    printInfoToLog("Packing the following Classes:\n")
     # Validate the classFile exist
     for className in classFiles:
         classFile = className.strip()
         if not isFileExist(classFile):
-            print("Java class in path: {} doesn't exist!\nFAILED ".format(classFile) )
+            printErrorToLog("Java class in path: {} doesn't exist!".format(classFile) )
             deleteFolder(_baseDir)
             exit()
 
@@ -136,13 +168,13 @@ with open(_fileName) as f:
                 print("{}/{}".format(targetFullDir, fn))
                 shutil.copy2(fullPathToClassFile, "{}".format(targetFullDir))
             else:
-                print("Couldn't create a new directory {}\nFAILED".format(targetFullDir))
+                printErrorToLog("Couldn't create a new directory {}".format(targetFullDir))
                 exit()
 
-    shutil.move(logFile, "{}/{}".format(_baseDir, logFile.replace(".log", ".txt"))
-    print("\nArchive Patch to: {}.zip".format(_baseDir))
+    shutil.move(logFile, "{}/{}".format(_baseDir, logFile.replace(".log", ".txt")))
+    printInfoToLog("\nArchive Patch to: {}.zip".format(_baseDir))
     archiveFolder(_baseDir, _baseDir)
-    print("Delete the src Directory\nPATCH - SUCCESS")
+    printInfoToLog("Delete the src Directory\nPATCH - SUCCESS")
     deleteFolder(_baseDir)
 
 log_file.close()
